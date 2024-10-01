507959fb21a278092320aaa1ed04ff2c376c4ccd
ben-sentenac <bsr-web-09@proton.me>
2024-10-01 20:34:50 +0200

    add parseFile function

---
+++ fixtures/task.md
@@ -0,0 +1,32 @@
+# Project Task List
+### Instructions Subtasks have three statuses:
+- `[-]` = Not Yet (task not started)
+- `[/]` = In Progress (task is currently being worked on)
+- `[x]` = Done (task completed)
+To update a subtask, change the symbol before the subtask to reflect its current progress.
+
+--- 
+
+# Task: Setup Environment123
+- [-] Install required software (Node.js, Git)
+- [/] Set up project repository
+- [x] Initialize the project with `npm init`
+
+# Task: Implement Basic Features
+- [-] Create task input form
+- [/] Add functionality to add new tasks
+- [x] Implement task deletion
+- [x] Add task completion toggle
+
+# Task: Style and Design
+- [-] Create basic layout and style using CSS
+- [-] Make the app responsive
+- [/] Add animations for task actions
+
+---
+### Task Template (<Progress Percentage>%)
+
+# Task: <Task Title>
+- [-] <Subtask 1>
+- [-] <Subtask 2>
+- [-] <Subtask 3>
---
+++ taskParse.ts
@@ -0,0 +1,57 @@
+import fs from 'node:fs/promises';
+import readline from 'node:readline/promises';
+import path from 'node:path';
+//
+const fileToParse = path.join('./fixtures/task.md');
+//
+async function parseFile(file: string) {
+    let fileHandle: fs.FileHandle;
+    const taskDict = {};
+    let currentTask: string | null = null;
+    let processingTask = false;//control the processing flow
+    try {
+        fileHandle = await fs.open(file, 'r');
+        //TODO can replace all readline piping by new convenient method fileHanlde.readlines()
+        const fileStream = fileHandle.createReadStream();
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity,
+        });
+        for await (const line of rl) {
+            const trimedLine = line.trim();
+            //parse the content betwenn 2 delimiter --- --- ignore before and after
+            if (trimedLine === '---') {
+                processingTask = !processingTask;
+                continue;
+            }
+            if (processingTask) {
+                const taskMatch = line.match(/^# Task: ([\w ]+)/);
+                const subtaskMatch = line.match(/^\s*-\s*\[\s*([-x/])\]\s*(.*)$/);
+                if (taskMatch) {
+                    const taskHeader = taskMatch[1];
+                    taskDict[taskHeader] = {
+                        subtask: []
+                    }
+                    //update currentTask
+                    currentTask = taskHeader;
+                }
+                if (subtaskMatch && currentTask) {
+                    const subtaskStatus = subtaskMatch[1];
+                    const subtaskName = subtaskMatch[2];
+                    console.log(subtaskName, subtaskStatus);
+                    taskDict[currentTask].subtask.push({
+                        name: subtaskName,
+                        status: subtaskStatus
+                    });
+                }
+            }
+        }
+        console.log(JSON.stringify(taskDict, null, 2));
+        return taskDict;
+    } catch (error) {
+        //TODO handling proper error
+        throw error;
+    }
+}
+//
+await parseFile(fileToParse);
